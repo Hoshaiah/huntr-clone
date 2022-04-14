8dd34fac6f28bd5ce7000636e0321f6be45f87df
nicopleyto <nicopleyto@gmail.com>
2022-04-14 14:54:30 +0800

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :kanbans
+  resources :kanbans do
+    resources :kanban_columns do
+      resources :cards
+    end
+  end
+
   devise_for :users
   root to: 'pages#home'
+  patch '/kanbans/:id/sort', to: 'kanbans#sort', as: "kanban_sort"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
