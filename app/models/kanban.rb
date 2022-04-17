cba5ca20ab75c766cdd9cfed738983ab585446c2
nicopleyto <nicopleyto@gmail.com>
2022-04-17 15:50:59 +0800

    added dependent destroy associations & create default columns method

--- app/models/kanban.rb
+++ app/models/kanban.rb
@@ -1,6 +1,18 @@
 class Kanban < ApplicationRecord
     belongs_to :user
-    has_many :kanban_columns
+    has_many :kanban_columns, dependent: :destroy
     has_many :cards, through: :kanban_columns
     has_many :activities, through: :cards
+
+    KANBAN_COLUMNS = ['WISHLIST','APPLIED','INTERVIEW','OFFER RECIEVED','OFFER ACCEPTED', 'OFFER REJECTED', 'REJECTED']
+
+    def create_default_columns
+        KANBAN_COLUMNS.each do |column|
+            self.kanban_columns.create(
+                name: column,
+                kanban: self
+            )
+        end 
+    end
+
 end
