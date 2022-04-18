40dd9c2227a14d9dd1e0a856d054e5761a0be939
nicopleyto <nicopleyto@gmail.com>
2022-04-18 13:28:02 +0800

    removed description column from kanban table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_14_191727) do
+ActiveRecord::Schema.define(version: 2022_04_18_052438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 2022_04_14_191727) do
 
   create_table "kanbans", force: :cascade do |t|
     t.string "name"
-    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
