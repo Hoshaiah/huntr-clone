5b2e4389825a63688617ac27c0828ecd0760216d
nicopleyto <nicopleyto@gmail.com>
2022-04-22 15:26:38 +0800

    testing graph

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,5 +1,7 @@
 class MetricsController < ApplicationController
   def index
     @kanban = current_user.kanbans.find(params[:kanban_id])
+    @graph_columns = @kanban.kanban_columns.where({ name: ["WISHLIST", "APPLIED", "INTERVIEW", "OFFER RECIEVED"]}).each
+    @jobs = @kanban.cards.order(updated_at: :desc)
   end
 end
