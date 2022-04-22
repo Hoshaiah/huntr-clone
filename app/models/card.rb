0d7149728b4f8a03a78cc74196f0c4719e6be13a
Hoshaiah <domingohoshaiah@gmail.com>
2022-04-22 15:16:56 +0800

    edits to kanban column name

--- app/models/card.rb
+++ app/models/card.rb
@@ -7,16 +7,16 @@ class Card < ApplicationRecord
     kanban_column = kanban.kanban_columns.find_by(id: column_id)
     kanban_column_name = kanban_column.name
 
-    if kanban_column_name == "Applied" && !self.activities.find_by(tag: "Apply")
+    if kanban_column_name == "APPLIED" && !self.activities.find_by(tag: "Apply")
       self.activities.create(tag: "Apply", title: "Applied", completed: true )
       return true
-    elsif kanban_column_name == "Interview"
+    elsif kanban_column_name == "INTERVIEW"
       self.activities.create(tag: "On Site Interview", title: "On Site Interview")
       return true
-    elsif kanban_column_name == "Offer Received" && !self.activities.find_by(tag: "Offer Received") 
+    elsif kanban_column_name == "OFFER RECEIVED" && !self.activities.find_by(tag: "Offer Received") 
       self.activities.create(tag: "Offer Received", title: "Offer Received", completed: true)
       return true
-    elsif kanban_column_name == "Offer Accepted" && !self.activities.find_by(tag: "Offer Accepted")  
+    elsif kanban_column_name == "OFFER ACCEPTED" && !self.activities.find_by(tag: "Offer Accepted")  
       self.activities.create(tag: "Offer Accepted", title: "Offer Accepted", completed: true)
       return true
     end
