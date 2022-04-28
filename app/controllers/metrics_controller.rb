3a78395a662ee84af723ebbaf0e3403ae1bbb445
nicopleyto <nicopleyto@gmail.com>
2022-04-28 17:14:19 +0800

    added graph for jobs created

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -9,6 +9,17 @@ class MetricsController < ApplicationController
     end
 
     @column_graph_data = @kanban.create_column_graph_data(params[:from_date], params[:to_date])
+
+    @jobs_created_data = return_jobs_created_data(@cards, params[:from_date], params[:to_date])
+  end
+
+  private
+
+  def return_jobs_created_data(cards, from_date, to_date)
+    from_date = from_date.nil? ? 1.month.ago.beginning_of_day : from_date.to_date.beginning_of_day
+    to_date = to_date.nil? ? Date.current.end_of_day : to_date.to_date.end_of_day
+
+    @cards.group_by_day(:created_at, format: '%d %b', range: from_date..to_date).count
   end
 
 end
